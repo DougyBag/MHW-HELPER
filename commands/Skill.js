96a3714753b55c89d5418513984fc97f10410699
DougyBag <44122466+DougyBag@users.noreply.github.com>
2018-10-14 19:39:37 +0200

    Update Skill.js
    added ";"

--- commands/Skill.js
+++ commands/Skill.js
@@ -1,6 +1,6 @@
-const Commande = require('./Command')
-const Util = require('../util/Utils')
-const request = require("request")
+const Commande = require('./Command');
+const Util = require('../util/Utils');
+const request = require("request");
 
 module.exports = class Skill extends Commande{
 
@@ -9,8 +9,8 @@ module.exports = class Skill extends Commande{
     };
 
     static action (message) {
-        let args = message.content.split(' ')
-        args.shift() //supprime le premier caractere : !skill
+        let args = message.content.split(' ');
+        args.shift(); //supprime le premier caractere : !skill
         let msg = Util.gatherString(args);
         giveSkill(msg,message);
     };
@@ -18,8 +18,8 @@ module.exports = class Skill extends Commande{
 
 function giveSkill(skill,message){
     request("https://mhw-db.com/skills", function(error,response,body) {
-        plop = JSON.parse(body);
-        plop.forEach(element => {
+        let bodyJSON = JSON.parse(body);
+        bodyJSON.forEach(element => {
             if (element.name === skill){
                 message.channel.send(element.description);
                 return;
